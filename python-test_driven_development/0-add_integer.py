5e92a0d05161addbbcaf12e040342c012f0de1b0
AAndrews-1982 <altonandrews@icloud.com>
2023-05-22 15:17:47 -0700

    A function that adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""module 0-add_integer
+
+adds the input ints. also accepts integer floats"""
+
+
+def add_integer(a, b=98):
+    """tests for type to be int or float,
+
+    then if they are and are divisible by 1, return their sum
+
+    if they are float, cast to int.
+    int(x) == floor of x in integer format
+    """
+    if a is None:
+        return
+    if isinstance(a, int) is False and \
+       isinstance(a, float) is False or \
+       a is None:
+        raise TypeError("a must be an integer")
+    if isinstance(b, int) is False and \
+       isinstance(b, float) is False or \
+       b is None:
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
