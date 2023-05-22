afef480f9365493a356c5732f92c41cdead46b45
AAndrews-1982 <altonandrews@icloud.com>
2023-05-22 10:32:23 -0700

    empty class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Module to define a square"""
 
-Square = __import__('0-square').Square
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+class Square:
+    """An empty class that defines a square"""
+    pass
