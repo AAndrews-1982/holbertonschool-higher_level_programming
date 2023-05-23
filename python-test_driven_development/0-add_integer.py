085efe9e118a34e6e9f6a2d236608bd3dfdd14f6
AAndrews-1982 <altonandrews@icloud.com>
2023-05-23 12:38:03 -0700

    A function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""Module that adds two integers.
-
-The module handles the addition of two numbers, which can be either
-integers or floats. If either of them is a float, it is cast to an integer.
-If either of them is not a number, a TypeError is raised.
-"""
+""" 0. Integers Addition - 0-add_integer.py """
 
 
 def add_integer(a, b=98):
-    """Adds two integers.
-
-    The function adds two numbers, which must be integers or floats.
-    If any of them is a float, it is cast to an integer before the
-    addition.
+    """
+    This function adds two integers
+    or floats casted to int
 
     Args:
-        a: The first number. Must be an integer or a float.
-        b: The second number. Must be an integer or a float.
-
-    Returns:
-        The sum of a and b.
+        a: first integer to add
+        b: second iteger to add
 
     Raises:
-        TypeError: If a or b are not integers or floats.
-    """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+        TypeError if a or b is not float or int
 
-    return int(a) + int(b)
+    Return:
+        The result of the operation
+    """
+
+    if a is None or type(a) not in [int, float] or abs(a) > float('inf'):
+        raise TypeError("a must be an integer")
+    if b is None or type(b) not in [int, float] or abs(b) > float('inf'):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
