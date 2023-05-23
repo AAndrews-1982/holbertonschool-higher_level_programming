ec9e6e94bde14ce23d21d73aca89463898634ff1
AAndrews-1982 <altonandrews@icloud.com>
2023-05-23 11:56:31 -0700

    A function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,18 +12,9 @@ def add_integer(a, b=98):
     if they are float, cast to int.
     int(x) == floor of x in integer format
     """
-    if a is None:
-        return
-    if isinstance(a, int) is False and \
-       isinstance(a, float) is False or \
-       a is None:
+ if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, int) is False and \
-       isinstance(b, float) is False or \
-       b is None:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+
+    return int(a) + int(b)
