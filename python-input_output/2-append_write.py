9bc46ff5c135705300de08c5b5e7a03f2f1e0447
AAndrews-1982 <altonandrews@icloud.com>
2023-05-30 11:24:51 -0700

    changes made to code output

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+Write a function that appends a string at
+    the end of a text file (UTF8) and returns
+    the number of characters added:
+
+Prototype: def append_write(filename="", text=""):
+If the file doesn’t exist, it should be created
+You must use the with statement
+You don’t need to manage file permission or file
+    doesn't exist exceptions.
+You are not allowed to import any module
+"""
+
+
 def append_write(filename="", text=""):
-    """
-    Append a string to the end of a text file and return the number of characters added.
-    If the file doesn't exist, it will be created.
-    """
-    with open(filename, mode="a", encoding="utf-8") as file:
-        # Open the file in append mode ("a") to append the text at the end
-        characters_added = file.write(text)
-        # Write the text to the file and get the number of characters added
-    return characters_added
+    """Function to append text to a file"""
+    with open(filename, 'a', encoding='utf-8') as appendfile:
+        appendfile.write(text)
+        return len(text)
