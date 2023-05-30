b4dff4e6ed38a5758701b535c17430ee283258b9
AAndrews-1982 <altonandrews@icloud.com>
2023-05-30 11:42:15 -0700

    line 7 was too long, moved to a new line

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,7 +4,8 @@ Write a function that creates an Object from a “JSON file”:
 
 Prototype: def load_from_json_file(filename):
 You must use the with statement
-You don’t need to manage exceptions if the JSON string doesn’t represent an object.
+You don’t need to manage exceptions if the
+JSON string doesn’t represent an object.
 You don’t need to manage file permissions / exceptions.
 """
 
