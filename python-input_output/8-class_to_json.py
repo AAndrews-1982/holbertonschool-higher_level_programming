9d6f9f5d0690398feceb553af5029ed26ce7bd66
AAndrews-1982 <altonandrews@icloud.com>
2023-05-30 12:15:24 -0700

    fixed PEP8 validation errors

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,21 +5,23 @@ for JSON serialization of an object.
 
 Prototype: def class_to_json(obj):
 obj is an instance of a Class
+
 All attributes of the obj Class are serializable: list, dictionary, string,
 integer, and boolean
+
 You are not allowed to import any module.
 """
+
 
 def class_to_json(obj):
     """
     Return the dictionary description with a simple data structure
     for JSON serialization.
 
-    Args:
-        obj (object): An instance of a Class.
+    Args: obj (object): An instance of a Class.
 
-    Returns:
-        dict: Dictionary description of the object with serializable attributes.
+    Returns: dict: Dictionary description of the object
+    with serializable attributes.
 
     """
     return obj.__dict__
