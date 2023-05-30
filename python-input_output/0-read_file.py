8345fb77b94c655ae308dfe985debf1ef6dd750f
AAndrews-1982 <altonandrews@icloud.com>
2023-05-30 11:07:16 -0700

    changes made to code output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout:
+
+Prototype: def read_file(filename=""):
+You must use the with statement
+You don’t need to manage file permission or file doesn't exist exceptions.
+You are not allowed to import any module
+"""
+
 def read_file(filename=""):
     """Read a text file and print its contents to stdout"""
     with open(filename, encoding="utf-8") as file:
