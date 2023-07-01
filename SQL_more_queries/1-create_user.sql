adf05ca14c2ade60dffa09c6475ec474ed1c2fd7
AAndrews-1982 <altonandrews@icloud.com>
2023-07-01 08:38:57 -0700

    script that creates MYSQL

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,6 +2,5 @@
 
 -- Check if the user 'user_0d_1' already exists, if not, create the user with all privileges
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '{password}';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
