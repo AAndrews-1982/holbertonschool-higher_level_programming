7fd7d5e2cebf57463f451b9692385e07c0d568e4
Your Name <youremail@example.com>
2023-07-28 08:44:08 -0700

    a script that prints two arguments passed to it, in the following format is

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const firstArg = args[0];
-const secondArg = args[1];
 
-if (firstArg === undefined || secondArg === undefined) {
-  console.log('Usage: node script.js <arg1> <arg2>');
+if (args.length !== 2) {
+  console.log("Usage: node script.js <arg1> <arg2>");
 } else {
-  console.log(`${firstArg} is ${secondArg}`);
+  const [arg1, arg2] = args;
+  console.log(`${arg1} is ${arg2}`);
 }
