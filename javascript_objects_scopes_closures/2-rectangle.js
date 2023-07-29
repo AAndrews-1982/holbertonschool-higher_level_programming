9b2f67607a7b70e384bf6ed26d48f7d5ac55e18b
Your Name <youremail@example.com>
2023-07-29 11:19:24 -0700

    corrected output and semistandard

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,28 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
     }
+  }
 
-    this.width = w;
-    this.height = h;
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
