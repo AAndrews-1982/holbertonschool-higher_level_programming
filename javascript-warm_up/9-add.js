06e2b01cbd514d08affd24aa40ae35d24d3a16e1
Your Name <youremail@example.com>
2023-07-29 08:08:20 -0700

    corrected output

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const add (a, b) => {
+const add = (a, b) => {
   return a + b;
-}
+};
 
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-if (isNaN(arg1) || isNaN(arg2)) {
+if (isNaN(num1) || isNaN(num2)) {
   console.log('NaN');
-} 	else {
-	cont result = add(num1, num2);	
-	console.log(result);
+} else {
+  const result = add(num1, num2);
+  console.log(result);
 }
