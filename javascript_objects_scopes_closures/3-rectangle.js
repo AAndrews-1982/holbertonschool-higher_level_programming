851409f3457a6229a2977e0c88f018834e031416
Your Name <youremail@example.com>
2023-07-29 11:27:20 -0700

    fixed output errors

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,34 +1,20 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.assign(this, {});
     }
-
-    this.width = w;
-    this.height = h;
   }
 
-  print() {
-    if (this.width <= 0 || this.height <= 0) {
-      return;
-    }
-
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 }
 
-const rectangle1 = new Rectangle(3, 5);
-const rectangle2 = new Rectangle(0, 5);
-const rectangle3 = new Rectangle(3, -3);
-const rectangle4 = new Rectangle(3);
-const rectangle5 = new Rectangle();
-
-rectangle1.print();
-rectangle2.print();
-rectangle3.print();
-rectangle4.print();
-rectangle5.print();
+module.exports = Rectangle;
