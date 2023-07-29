e7d11a473332dddefbdd6fa987f0987367360d9d
Your Name <youremail@example.com>
2023-07-29 07:51:48 -0700

    a script that prints x times C is fun

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const lines = ['C is fun", 'Python is cool', 'JavaScript is amazing'];
+#!/usr/bin/env node
 
-for (let i = 0; i < lines.length; i++) {
-  console.log(lines[i]);
-}
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+lines.forEach((line) => console.log(line));
