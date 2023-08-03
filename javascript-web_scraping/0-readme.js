7d11fdf6a442c8f310352572b3f67de4923bfef0
Your Name <youremail@example.com>
2023-08-03 16:18:28 -0700

    fixed output error

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,17 +4,23 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-function readAndPrintFile(filePath) {
-    try {
-        const content = fs.readFileSync(filePath, 'utf-8');
-        console.log(content);
-    } catch (error) {
-        console.error('An error occurred:', error.message);
+function readAndPrintFile (filePath) {
+  try {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log(content);
+  } catch (error) {
+       if (error.code === 'ENOENT') {
+	       console.error('File not found:', filePath);
+       } else if (!error.message.includes('Error')) {
+	       console.error('An unexpected error occured:', error.message);
+       } else {
+    console.error('An error occurred:', error.message);
     }
+  } 
 }
 
 if (!filePath) {
-    console.log('Usage: ./script_name.js <file_path>');
+  console.log('Usage: ./script_name.js <file_path>');
 } else {
-    readAndPrintFile(filePath);
+  readAndPrintFile(filePath);
 }
