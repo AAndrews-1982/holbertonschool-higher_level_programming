af1539c1746aec373570d649074868940db3b837
Your Name <youremail@example.com>
2023-08-03 17:09:18 -0700

    corrected output error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const [fileToWrite, contentToWrite] = process.argv.slice(2);
 
-const [filePath, content] = process.argv.slice(2);
-
-if (!filePath || !content) return console.log('Usage: ./script_name.js <file_path> <content>');
-
-try {
-    fs.writeFileSync(filePath, content, 'utf-8');
-    console.log("Content written successfully.");
-} catch (error) {
+fs.writeFile(fileToWrite, contentToWrite, 'utf-8', (error) => {
+  if (error) {
     console.error('An error occurred:', error.message);
-}
+  } else {
+    console.log('Content written successfully.');
+  }
+});
