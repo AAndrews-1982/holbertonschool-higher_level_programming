6f62524ec7462cbbee13b4c4a5991e306c29c1de
Your Name <youremail@example.com>
2023-08-03 17:04:09 -0700

    script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,20 +2,13 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const content = process.argv[3];
+const [filePath, content] = process.argv.slice(2);
 
-function writeToFile(filePath, content) {
-    try {
-        fs.writeFileSync(filePath, content, 'utf-8');
-        console.log("Content written successfully.");
-    } catch (error) {
-        console.error('An error occurred:', error.message);
-    }
-}
+if (!filePath || !content) return console.log('Usage: ./script_name.js <file_path> <content>');
 
-if (!filePath || !content) {
-    console.log('Usage: ./script_name.js <file_path> <content>');
-} else {
-    writeToFile(filePath, content);
+try {
+    fs.writeFileSync(filePath, content, 'utf-8');
+    console.log("Content written successfully.");
+} catch (error) {
+    console.error('An error occurred:', error.message);
 }
