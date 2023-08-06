57a16500d90bb4e3c40b021633768b40954c9d79
Your Name <youremail@example.com>
2023-08-06 16:33:50 -0700

    fixed output errors

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fs = require('fs/promises'); // Using the built-in promises version of fs
+const fs = require('fs'); // Using the built-in fs module
 
 const url = process.argv[2];
 const filePath = process.argv[3];
@@ -9,14 +9,13 @@ const filePath = process.argv[3];
 request(url, (error, response, body) => {
   if (error) {
     console.error('error:', error);
-    return;
-  }
-
-  fs.writeFile(filePath, body, 'utf8')
-    .then(() => {
-      console.log(`Contents of ${url} have been saved to ${filePath}`);
-    })
-    .catch(err => {
-      console.error(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf8', function (err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Contents of ${url} have been saved to ${filePath}`);
+      }
     });
+  }
 });
