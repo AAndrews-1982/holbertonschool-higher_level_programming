420f2f05488ec97620ec10a68175ff785c946ebb
Your Name <youremail@example.com>
2023-08-06 16:37:02 -0700

    fixed output errors for the 1000th time

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,35 +1,31 @@
 #!/usr/bin/node
-
 const request = require('request');
+const url = process.argv[2];
+let counter = 0;
+const dictionary = {};
+let user = 0;
 
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('An error occurred:', error.message);
-    return;
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
-    return;
-  }
-
-  const todos = JSON.parse(body);
-
-  const completedTasksByUser = {};
-
-  for (const todo of todos) {
-    if (todo.completed) {
-      if (completedTasksByUser[todo.userId]) {
-        completedTasksByUser[todo.userId]++;
-      } else {
-        completedTasksByUser[todo.userId] = 1;
+  if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    for (const task of data) {
+      if (user !== task.userId) {
+        user = task.userId;
+        counter = 0;
+      }
+      if (task.completed) {
+        counter++;
+      }
+      if (counter !== 0) {
+        dictionary[user] = counter;
       }
     }
-  }
-
-  for (const userId in completedTasksByUser) {
-    console.log(`User ${userId} completed ${completedTasksByUser[userId]} tasks.`);
+    console.log(dictionary);
+  } else {
+    console.error('Request failed with status:', response.statusCode);
   }
 });
