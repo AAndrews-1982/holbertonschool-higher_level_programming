9245747bbb97e67af4504a28e58626de3708bbb5
Your Name <youremail@example.com>
2023-08-06 16:51:07 -0700

    JavaScript script that updates the text color of the <header> element to red (#FF0000)

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Select the <header> element using document.querySelector
+const headerElement = document.querySelector('header');
+
+// Update the text color of the selected <header> element
+headerElement.style.color = '#FF0000';
