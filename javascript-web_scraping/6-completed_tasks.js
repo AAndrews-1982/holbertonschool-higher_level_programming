0d1a3c15da5447867d067f0ff0b45bdff82b16b4
Your Name <youremail@example.com>
2023-08-06 16:47:32 -0700

    fixed output error, again

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,35 +2,24 @@
 
 const request = require('request');
 const url = process.argv[2];
-let counter = 0;
-const dictionary = {};
-let user = 0;
 
-request.get(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  }
-
-  if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    for (const task of data) {
-      if (user !== task.userId) {
-        if (counter !== 0) {
-          dictionary[user] = counter;
-        }
-        user = task.userId;
-        counter = 0;
-      }
-      if (task.completed) {
-        counter++;
-      }
-    }
-    // To store the count for the last user
-    if (counter !== 0) {
-      dictionary[user] = counter;
-    }
-    console.log(dictionary);
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
   } else {
-    console.error('Request failed with status:', response.statusCode);
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(task => {
+      if (task.completed) {
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 1;
+        } else {
+          completedTasks[task.userId]++;
+        }
+      }
+    });
+
+    console.log(completedTasks);
   }
 });
